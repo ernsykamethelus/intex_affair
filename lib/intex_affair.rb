84a0c89650785d51f95f5872843505e79c667342
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-10-08 02:51:32 +0000

    add description

---
+++ lib/intex_affair.rb
@@ -0,0 +1,6 @@
+require "intex_affair/version"
+
+module IntexAffair
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/intex_affair/version.rb
@@ -0,0 +1,3 @@
+module IntexAffair
+  VERSION = "0.1.0"
+end
